5a40ef90e1887f6633a9d023a292a37aac1ef346
ArtIKim <null_number_13@hotmail.com>
2021-04-08 14:20:41 +0300

    08042021_3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,48 @@
-const darkModeButton = document.querySelector(".js-switch-button-dark-mode");
+// const darkModeButton = document.querySelector(".js-switch-button-dark-mode");
+// const switchBallDarkMode = document.querySelector(".css-switch-ball-dark-mode");
 
-darkModeButton.addEventListener("change", () => {
-	document.body.classList.toggle("dark-mode");
+// darkModeButton.addEventListener("change", () => {
+// 	switchBallDarkMode.classList.toggle("active");
+// 	document.body.classList.toggle("dark-mode");
+
+// 	if (document.body.classList.contains("dark-mode")) {
+// 		localStorage.setItem("dark-mode", "true");
+// 		localStorage.setItem("switch-button-dark-mode", "true");
+// 	} else {
+// 		localStorage.setItem("dark-mode", "false");
+// 		localStorage.setItem("switch-button-dark-mode", "false");
+// 	}
+// });
+
+// if (localStorage.getItem("dark-mode") === "true") {
+// 	document.body.classList.add("dark-mode");
+// 	switchBallDarkMode.classList.add("active");
+// } else {
+// 	document.body.classList.remove("dark-mode");
+// 	switchBallDarkMode.classList.remove("active");
+// }
+
+// const switchButton = document.querySelector(".css-switch-label");
+
+document.addEventListener("click", (e) => {
+	if (e.target.className === "css-switch-input") {
+		const switchBall = e.target.nextElementSibling.nextElementSibling;
+		const setSwitchButtons = [];
+
+		switchBall.classList.toggle("active");
+
+		const id = e.target.id;
+		const status = switchBall.classList.contains("active") ? "active" : "disabled";
+
+		const stateButton = {
+			id,
+			status,
+		};
+
+		setSwitchButtons.push(stateButton);
+
+		console.log(setSwitchButtons);
+
+		localStorage.setItem("switch-buttons-status", JSON.stringify(setSwitchButtons));
+	}
 });
