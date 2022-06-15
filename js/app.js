66a8ff64798d3225d49013cd1b96f73fbddba440
AnnaKitou <null_number_13@hotmail.com>
2022-06-15 21:51:35 +0300

    with tailwindcss v3

--- js/app.js
+++ js/app.js
@@ -1,48 +1,11 @@
-// const darkModeButton = document.querySelector(".js-switch-button-dark-mode");
-// const switchBallDarkMode = document.querySelector(".css-switch-ball-dark-mode");
+const setMargin = () => {
+	// const classes = [...document.querySelector("body").children].map((item) => item.tagName).filter((item) => item !== "SCRIPT");
+	// const classes = [...document.querySelector("body").children].map((item) => item.classList);
+	const classes = document.getElementsByClassName("mt-10");
 
-// darkModeButton.addEventListener("change", () => {
-// 	switchBallDarkMode.classList.toggle("active");
-// 	document.body.classList.toggle("dark-mode");
+	console.log(classes);
+};
 
-// 	if (document.body.classList.contains("dark-mode")) {
-// 		localStorage.setItem("dark-mode", "true");
-// 		localStorage.setItem("switch-button-dark-mode", "true");
-// 	} else {
-// 		localStorage.setItem("dark-mode", "false");
-// 		localStorage.setItem("switch-button-dark-mode", "false");
-// 	}
-// });
-
-// if (localStorage.getItem("dark-mode") === "true") {
-// 	document.body.classList.add("dark-mode");
-// 	switchBallDarkMode.classList.add("active");
-// } else {
-// 	document.body.classList.remove("dark-mode");
-// 	switchBallDarkMode.classList.remove("active");
-// }
-
-// const switchButton = document.querySelector(".css-switch-label");
-
-document.addEventListener("click", (e) => {
-	if (e.target.className === "css-switch-input") {
-		const switchBall = e.target.nextElementSibling.nextElementSibling;
-		const setSwitchButtons = [];
-
-		switchBall.classList.toggle("active");
-
-		const id = e.target.id;
-		const status = switchBall.classList.contains("active") ? "active" : "disabled";
-
-		const stateButton = {
-			id,
-			status,
-		};
-
-		setSwitchButtons.push(stateButton);
-
-		console.log(setSwitchButtons);
-
-		localStorage.setItem("switch-buttons-status", JSON.stringify(setSwitchButtons));
-	}
+document.addEventListener("DOMContentLoaded", () => {
+	setMargin();
 });
